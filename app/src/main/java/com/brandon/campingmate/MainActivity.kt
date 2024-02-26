6fddddca834b367eb3e56ea090d0ed777c10fc54
quinones <127173772+WjLeeeee@users.noreply.github.com>
2024-02-26 21:28:14 +0900

    프로그램 초기세팅&아이콘추가 (#2)
    * [Build] 의존성 추가
    
    의존성 추가 및 기본 설정값 변경
    
    * [Design] vector이미지 추가
    
    프로젝트에 사용될 벡터 이미지들 추가

--- app/src/main/java/com/brandon/campingmate/MainActivity.kt
+++ app/src/main/java/com/brandon/campingmate/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.brandon.campingmate
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.brandon.campingmate.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val databaseReference: DatabaseReference = firebaseDatabase.getReference()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            databaseReference.child("message").child("woojin").setValue(123)
+        }
     }
 }
