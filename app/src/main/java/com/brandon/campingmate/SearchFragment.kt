83046f2aed3fd154190101164a7a7bc098ad63de
WjLeeeee <dldnwls0115@naver.com>
2024-02-29 21:50:23 +0900

    [Remove] 필요없는파일 제거
    바텀시트 ui보기 위해서 만들었던 파일 제거, 바텀시트 버튼디자인 수정

--- app/src/main/java/com/brandon/campingmate/SearchFragment.kt
+++ app/src/main/java/com/brandon/campingmate/SearchFragment.kt
@@ -1,7 +1,6 @@
 package com.brandon.campingmate
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +9,6 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.brandon.campingmate.databinding.FragmentSearchBinding
-import com.brandon.campingmate.databinding.SearchBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.Firebase
 import com.google.firebase.database.FirebaseDatabase
