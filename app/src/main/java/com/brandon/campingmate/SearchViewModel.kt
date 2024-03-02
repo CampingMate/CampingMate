9ab5e5ca4f6ab9c0217f4412240ba4e4175e4726
WjLeeeee <dldnwls0115@naver.com>
2024-03-02 17:35:37 +0900

    [Feat] 검색프래그먼트 생성
    검색프래그먼트 구조 생성, 바텀시트 필터생성, 필터링 기능은 구현X

--- app/src/main/java/com/brandon/campingmate/SearchViewModel.kt
+++ app/src/main/java/com/brandon/campingmate/SearchViewModel.kt
@@ -1,6 +1,10 @@
 package com.brandon.campingmate
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SearchViewModel: ViewModel() {
+//    private val _resetBtn: MutableLiveData<> = MutableLiveData()
+//    val resetBtn: LiveData<> get() = _resetBtn
 }
