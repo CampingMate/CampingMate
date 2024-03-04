bf94e8e84899b8e17470b36987f51c921a79daab
avo890 <wizhey7327@gmail.com>
2024-03-04 22:12:15 +0900

    [Feat] 프로필 이미지/이름 수정
    - 프로필 수정 이미지 선택 인텐트 연결
    - 프로필 수정 취소 확인 중복 코드 리팩토링
    - 프로필 이름 변경 다이얼로그 연결 : 다이얼로그 영역 화면에 꽉차게 조정 + 화면 dim설정 0.9로 설정
    - 이름 글자수 10자 제한 및 글자수 추적 가능
    - 로그인 시 탭에서 로그인 권유 문구가 나오는 문제 수정

--- app/src/main/java/com/brandon/campingmate/ViewPager2Adapter.kt
+++ app/src/main/java/com/brandon/campingmate/ViewPager2Adapter.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.brandon.campingmate.presentation.profile.ProfileFragment
 
 class ViewPager2Adapter(
     fragmentManager: FragmentManager,
