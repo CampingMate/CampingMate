84c9eb0242d32339ce68b57e7e38d922534acd52
Keunyoung Song <rmsdud623@gmail.com>
2024-03-04 14:23:46 +0900

    [Refactor] 프로젝트 구조 변경  (#17)
    * [Fix] Fix memory leak from map api
    
    * [Refactor] Change project structure

---
+++ app/src/main/java/com/brandon/campingmate/PostModel.kt
@@ -0,0 +1,16 @@
+package com.brandon.campingmate
+
+import com.google.firebase.Timestamp
+
+data class PostModel(
+    val author: String? = null,
+    val authorId: String? = null,
+    val authorProfileImageUrl: String? = null,
+    val title: String? = null,
+    val content: String? = null,
+    val imageUrlList: List<String>? = null,
+    val timestamp: Timestamp? = null //
+)
+
+//    val timestamp: Timestamp? = Timestamp.now() // Firestore의 Timestamp 사용
+
--- app/src/main/java/com/brandon/campingmate/ViewPager2Adapter.kt
+++ app/src/main/java/com/brandon/campingmate/ViewPager2Adapter.kt
@@ -4,11 +4,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.brandon.campingmate.ui.BoardFragment
-import com.brandon.campingmate.ui.ChatFragment
-import com.brandon.campingmate.ui.HomeFragment
-import com.brandon.campingmate.ui.MapFragment
-import com.brandon.campingmate.ui.ProfileFragment
 
 class ViewPager2Adapter(
     fragmentManager: FragmentManager,
