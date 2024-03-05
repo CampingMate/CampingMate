0eeceb7da5353d6d3e32ca47015b1dba43f8fee8
quinones <127173772+WjLeeeee@users.noreply.github.com>
2024-03-05 23:01:42 +0900

    [Feat] search scroll tab, searchApi (#30)
    * [Feat] 검색프래그먼트 생성
    
    검색프래그먼트 구조 생성, 바텀시트 필터생성, 필터링 기능은 구현X
    
    * [Feat] 검색프래그먼트 필터 구현
    
    필터수정, 필터 칩 선택후 적용하기누르면 리사이클러뷰에 데이터1개 추가.
    
    * [Design] CampDetailActivity ui design
    
    캠프디테일 액티비티 뼈대 구현
    
    * [Feat] CampDetailActivity scrollTab
    
    캠프디테일 스크롤탭 구현, Todo: 이미지 불러오기 수정
    
    * [Feat] 이미지검색 api 연동
    
    이미지검색 공공데이터 api이용해서 연동. todo: null로 불러와지는 오류
    
    * [Feat] 검색 바텀시트 scrollTab구현, 검색api연동
    
    검색 바텀시트 유형 선택시 스크롤 구현, 검색api연동

--- app/src/main/java/com/brandon/campingmate/CampDetailActivity.kt
+++ app/src/main/java/com/brandon/campingmate/CampDetailActivity.kt
@@ -2,13 +2,91 @@ package com.brandon.campingmate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.brandon.campingmate.databinding.ActivityCampDetailBinding
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.launch
 
 class CampDetailActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityCampDetailBinding.inflate(layoutInflater) }
+    private val viewModel by lazy {
+        ViewModelProvider(this)[CampDetailViewModel::class.java]
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        initView()
+        initViewModel()
+    }
+
+    private fun initViewModel() =with(viewModel) {
+        imageParam.observe(this@CampDetailActivity){
+            viewModel.communicateNetWork(it)
+        }
+        imageResult.observe(this@CampDetailActivity){
+            if(it.isNotEmpty()){
+                val firstImageUrl = it[0]
+                Glide.with(applicationContext).load(firstImageUrl).into(binding.ivDetail)
+            }
+        }
+    }
+
+    private fun initView() =with(binding) {
+        val myData = intent.getParcelableExtra("campData") as? CampModel
+        if(myData != null){
+            val contentId = myData.contentId.toString()
+            tvCampName.text = myData.facltNm
+            tvAddr.text = myData.addr1
+            tvCall.text = myData.tel
+            tvHomepage.text = "홈페이지 - ${myData.homepage}"
+            tvSize.text = "면적 - ${myData.allar}㎡"
+            tvRestTime.text = "휴장기간 ${myData.hvofBgnde} ~ ${myData.hvofEnddle} "
+            tvPlayTime.text = "운영기간 - ${myData.operPdCl}"
+            var bottom = ""
+            if(myData.siteBottomCl1 != "0") bottom += "잔디, "
+            if(myData.siteBottomCl2 != "0") bottom += "파쇄석, "
+            if(myData.siteBottomCl3 != "0") bottom += "테크, "
+            if(myData.siteBottomCl4 != "0") bottom += "자갈, "
+            if(myData.siteBottomCl5 != "0") bottom += "맨흙, "
+            tvBottom.text = "바닥재질 - ${bottom}"
+            tvIntroduceComment.text = myData.intro
+            tvConvenienceComment.text = "편의시설 - 화장실: ${myData.toiletCo} 샤워실: ${myData.swrmCo} 개수대: ${myData.wtrplCo} 화로대-${myData.brazierCl}"
+            tvConvenienceComment2.text = "부대시설 - ${myData.sbrsCl}"
+            tvConvenienceThema.text = "테마 - ${myData.themaEnvrnCl}"
+            tvConvenienceNear.text = "주변이용가능시설 - ${myData.posblFcltyCl}"
+            tvConvenienceFeature.text = "특징 - ${myData.featureNm}"
+            viewModel.setUpParkParameter(contentId)
+        }
+        ivArrowBack.setOnClickListener {
+            finish()
+        }
+        scrollTab()
+    }
+
+    private fun scrollTab() =with(binding){
+        tvInformationTab.setOnClickListener {
+            scrollToView(tvInformation)
+        }
+        tvIntroduceTab.setOnClickListener {
+            scrollToView(tvIntroduce)
+        }
+        tvConvenienceTab.setOnClickListener {
+            scrollToView(tvConvenience)
+        }
+        tvMapTab.setOnClickListener {
+            scrollToView(tvMap)
+        }
+        tvCommentTab.setOnClickListener {
+            scrollToView(tvComment)
+        }
+    }
+    private fun scrollToView(view: View) {
+        binding.scrollView.post {
+            binding.scrollView.smoothScrollTo(0, view.top)
+        }
     }
 }
---
+++ app/src/main/java/com/brandon/campingmate/CampDetailViewModel.kt
@@ -0,0 +1,47 @@
+package com.brandon.campingmate
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class CampDetailViewModel : ViewModel() {
+
+    private val _imageParam: MutableLiveData<HashMap<String, String>> = MutableLiveData()
+    val imageParam: LiveData<HashMap<String, String>> get() = _imageParam
+    private val _imageResult: MutableLiveData<MutableList<String>> = MutableLiveData()
+    val imageResult: LiveData<MutableList<String>> get() = _imageResult
+    fun setUpParkParameter(contentId: String) {
+        val authKey =
+            "wDP6fsVX3kKuaOD7OKrRHaAgPUNtxYUy387PNJRBAW/F6GUdZgv5LyyIAkVXED3leDg3aUD+TFIgBHWCgMBdzQ=="
+        _imageParam.value = hashMapOf(
+            "numOfRows" to "10",
+            "pageNo" to "1",
+            "MobileOS" to "AND",
+            "MobileApp" to "CampingMate",
+            "serviceKey" to authKey,
+            "_type" to "json",
+            "contentId" to contentId
+        )
+    }
+
+    fun communicateNetWork(param: HashMap<String, String>?) {
+        viewModelScope.launch {
+            val responseData = param?.let { NetWorkClient.imageNetWork.getImage(it) }
+            val items = responseData?.response?.campBody?.campImageItems?.campImageItem
+            val imageUrls = mutableListOf<String>()
+            if (items != null) {
+                for(item in items){
+                    Log.d("woojinCheck", "contentId : ${item.contentId} serialnum : ${item.serialnum} imageURL : ${item.imageurl}")
+                    val imageUrl = item.imageurl
+                    if (imageUrl != null) {
+                        imageUrls.add(imageUrl)
+                    }
+                }
+            }
+            _imageResult.value = imageUrls
+        }
+    }
+}
