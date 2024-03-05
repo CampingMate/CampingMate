bb4d6404a069a29673f1a3247bd1af9cd3671fe0
quinones <127173772+WjLeeeee@users.noreply.github.com>
2024-03-05 17:14:28 +0900

    Feat/campdetail UI (#25)
    * [Feat] 검색프래그먼트 생성
    
    검색프래그먼트 구조 생성, 바텀시트 필터생성, 필터링 기능은 구현X
    
    * [Feat] 검색프래그먼트 필터 구현
    
    필터수정, 필터 칩 선택후 적용하기누르면 리사이클러뷰에 데이터1개 추가.
    
    * [Design] CampDetailActivity ui design
    
    캠프디테일 액티비티 뼈대 구현

--- app/src/main/java/com/brandon/campingmate/SearchListAdapter.kt
+++ app/src/main/java/com/brandon/campingmate/SearchListAdapter.kt
@@ -1,16 +1,20 @@
 package com.brandon.campingmate
 
+import android.content.Intent
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.brandon.campingmate.SearchFragment.Companion.campList
 import com.brandon.campingmate.databinding.ItemBigCampBinding
 import com.bumptech.glide.Glide
 
 class SearchListAdapter(
-) : ListAdapter<CampModel, SearchListAdapter.VideoViewHolder>(
+) : ListAdapter<CampModel, SearchListAdapter.SearchViewHolder>(
     object : DiffUtil.ItemCallback<CampModel>(){
         override fun areItemsTheSame(
             oldItem: CampModel,
@@ -26,22 +30,22 @@ class SearchListAdapter(
         ): Boolean = oldItem == newItem
     }
 ){
-    abstract class VideoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    abstract class SearchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         abstract fun onBind(item: CampModel)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder =
-        VideoItemViewHolder(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder =
+        SearchItemViewHolder(
             ItemBigCampBinding.inflate(LayoutInflater.from(parent.context), parent, false),
         )
 
-    override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
         holder.onBind(getItem(position))
     }
 
-    class VideoItemViewHolder(
+    class SearchItemViewHolder(
         private val binding: ItemBigCampBinding,
-    ) : VideoViewHolder(binding.root){
+    ) : SearchViewHolder(binding.root){
         override fun onBind(item: CampModel) = with(binding){
             if(item !is CampModel) {
                 return@with
@@ -52,6 +56,21 @@ class SearchListAdapter(
             tvBigItemLineIntro.text = item.lineIntro
             tvBigItemTag.text = item.lctCl.toString()
             ivBigItem.clipToOutline = true
+
+            binding.root.setOnClickListener {
+                val myData = CampModel(addr1=item.addr1, contentId = item.contentId, facltNm = item.facltNm,
+                    wtrplCo = item.wtrplCo, brazierCl = item.brazierCl, sbrsCl = item.sbrsCl, posblFcltyCl = item.posblFcltyCl,
+                    hvofBgnde = item.hvofBgnde, hvofEnddle = item.hvofEnddle, toiletCo = item.toiletCo, swrmCo = item.swrmCo,
+                    featureNm = item.featureNm, induty = item.induty, tel = item.tel, homepage = item.homepage, resveCl = item.resveCl,
+                    siteBottomCl1 = item.siteBottomCl1, siteBottomCl2 = item.siteBottomCl2, siteBottomCl3 = item.siteBottomCl3,
+                    siteBottomCl4 = item.siteBottomCl4, siteBottomCl5 = item.siteBottomCl5, glampInnerFclty = item.glampInnerFclty,
+                    caravInnerFclty = item.caravInnerFclty, intro = item.intro, themaEnvrnCl = item.themaEnvrnCl)
+
+                val intent = Intent(binding.root.context, CampDetailActivity::class.java).apply {
+                    putExtra("campData", myData)
+                }
+                binding.root.context.startActivity(intent)
+            }
         }
     }
 }
