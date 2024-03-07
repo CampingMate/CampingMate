99c21288a08b3a5b3e6300ffce1faa0eadd10d2e
Jihye Moon <111625704+avo890@users.noreply.github.com>
2024-03-07 12:17:50 +0900

    [Fix] 존재하는 유저의 로그인 정보 초기화 문제 해결 (#50)
    - 로그인할 때마다 데이터베이스 유저정보에서 정보 초기화 되는 문제 해결
    - 이미 존재하는 유저의 데이터베이스 정보가 유지 되게 구현

--- app/src/main/java/com/brandon/campingmate/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/brandon/campingmate/presentation/login/LoginActivity.kt
@@ -24,33 +24,40 @@ class LoginActivity : AppCompatActivity() {
                     error.toString() == AuthErrorCause.AccessDenied.toString() -> {
                         Toast.makeText(this, "접근이 거부 됨(동의 취소)", Toast.LENGTH_SHORT).show()
                     }
+
                     error.toString() == AuthErrorCause.InvalidClient.toString() -> {
                         Toast.makeText(this, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
                     }
+
                     error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
                         Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT).show()
                     }
+
                     error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
                         Toast.makeText(this, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
                     }
+
                     error.toString() == AuthErrorCause.InvalidScope.toString() -> {
                         Toast.makeText(this, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
                     }
+
                     error.toString() == AuthErrorCause.Misconfigured.toString() -> {
                         Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT).show()
                     }
+
                     error.toString() == AuthErrorCause.ServerError.toString() -> {
                         Toast.makeText(this, "서버 내부 에러", Toast.LENGTH_SHORT).show()
                     }
+
                     error.toString() == AuthErrorCause.Unauthorized.toString() -> {
                         Toast.makeText(this, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
                     }
+
                     else -> { // Unknown
                         Toast.makeText(this, "기타 에러", Toast.LENGTH_SHORT).show()
                     }
                 }
-            }
-            else if (token != null) {
+            } else if (token != null) {
                 Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
 //                val intent = Intent(this, MainActivity::class.java)
 //                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
@@ -60,22 +67,27 @@ class LoginActivity : AppCompatActivity() {
                         "nickName" to "${user?.kakaoAccount?.profile?.nickname}",
                         "profileImage" to "${user?.kakaoAccount?.profile?.profileImageUrl}",
                         "userEmail" to "${user?.kakaoAccount?.email}",
-                        "boomarked" to null,
+                        "bookmarked" to "null",
                         "writing" to null
                     )
-                    db.collection("users").document("Kakao${user?.id}").set(userModel)
+                    val documentRef = db.collection("users").document("Kakao${user?.id}")
+                    documentRef.get().addOnSuccessListener {
+                        if (!it.exists()) {
+                            documentRef.set(userModel)
+                        }
+                    }
+                    finish()
                 }
-                finish()
             }
         }
         val kakao_login_button = binding.cvKakao
         //로그인하기 버튼 클릭시
-        kakao_login_button.setOnClickListener{
+        kakao_login_button.setOnClickListener {
             //카카오톡이 설치되어 있으면, 카카오톡로그인
-            if(LoginClient.instance.isKakaoTalkLoginAvailable(this)){
+            if (LoginClient.instance.isKakaoTalkLoginAvailable(this)) {
                 LoginClient.instance.loginWithKakaoTalk(this, callback = callback)
                 //설치되어 있지 않으면, 카카오 계정 로그인(웹)
-            }else{
+            } else {
                 LoginClient.instance.loginWithKakaoAccount(this, callback = callback)
             }
         }
