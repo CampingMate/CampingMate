ea1229fbf2f5953392868c7f4277c2e2bd901a5c
Keunyoung Song <rmsdud623@gmail.com>
2024-03-07 21:02:51 +0900

    [Feat]PostDetail Id 로 PostEntity 가져오기 구현 (#58)
    * [Feat] Implement getPostById() in PostDetail

--- app/src/main/java/com/brandon/campingmate/data/model/response/PostListResponse.kt
+++ app/src/main/java/com/brandon/campingmate/data/model/response/PostListResponse.kt
@@ -1,6 +1,5 @@
 package com.brandon.campingmate.data.model.response
 
-import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentSnapshot
 
 /**
@@ -10,17 +9,6 @@ import com.google.firebase.firestore.DocumentSnapshot
 data class PostListResponse(
     val posts: List<PostResponse>,
     var lastVisibleDoc: DocumentSnapshot?
-)
-
-data class PostResponse(
-    var postId: String? = null, // id 업데이트 필요(초기 업로드 시 id 없음)
-    val authorName: String? = null,
-    val authorId: String? = null,
-    val authorProfileImageUrl: String? = null,
-    val title: String? = null,
-    val content: String? = null,
-    val imageUrlList: List<String>? = null,
-    val timestamp: Timestamp? = null //
 )
 
 
---
+++ app/src/main/java/com/brandon/campingmate/presentation/postdetail/PostDetailEvent.kt
@@ -0,0 +1,3 @@
+package com.brandon.campingmate.presentation.postdetail
+
+sealed class PostDetailEvent
