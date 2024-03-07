17450d6e5edb4a8619f0d3ce451861ef71b90f6b
csh-tiger <94032024+csh-tiger@users.noreply.github.com>
2024-03-07 22:07:24 +0900

    [Feat] HomeFragment 칩 기능 구현 (#64)

--- app/src/main/java/com/brandon/campingmate/presentation/home/adapter/PetAdapter.kt
+++ app/src/main/java/com/brandon/campingmate/presentation/home/adapter/PetAdapter.kt
@@ -7,21 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.brandon.campingmate.R
-import com.brandon.campingmate.databinding.ItemSmallCampBinding
+import com.brandon.campingmate.databinding.ItemMiddleCampBinding
 import com.brandon.campingmate.domain.model.CampEntity
 import com.brandon.campingmate.presentation.campdetail.CampDetailActivity
 import com.bumptech.glide.Glide
 
-class HomeAdapter(private val mContext: Context, var mItems: MutableList<CampEntity?>) :
-    RecyclerView.Adapter<HomeAdapter.Holder>() {
+class PetAdapter(private val mContext: Context, var mItems: MutableList<CampEntity?>) :
+    RecyclerView.Adapter<PetAdapter.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val binding =
-            ItemSmallCampBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemMiddleCampBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return Holder(binding)
     }
 
-    override fun onBindViewHolder(holder: HomeAdapter.Holder, position: Int) {
+    override fun onBindViewHolder(holder: PetAdapter.Holder, position: Int) {
         holder.itemView.setOnClickListener {
             val intent = Intent(mContext, CampDetailActivity::class.java).apply {
                 putExtra("campData", mItems[position])
@@ -37,18 +36,15 @@ class HomeAdapter(private val mContext: Context, var mItems: MutableList<CampEnt
         }
 
         holder.campName.text = mItems[position]?.facltNm
+        holder.campIntro.text = mItems[position]?.lineIntro
     }
 
     override fun getItemCount() = mItems.size
 
-    inner class Holder(binding: ItemSmallCampBinding) : RecyclerView.ViewHolder(binding.root) {
-        val campImg = binding.ivHomeCampImg
-        val campName = binding.tvTitle
-        //후기 캠핑장
-//        val campCategory
-//        val campAddr
-//        val campReviewNum
-//        val campReview
+    inner class Holder(binding: ItemMiddleCampBinding) : RecyclerView.ViewHolder(binding.root) {
+        val campImg = binding.ivCampPetImg
+        val campName = binding.tvCampName
+        val campIntro = binding.tvLineIntro
 //        //반려동물 캠핑장
 //        val campCity
 //        val campLineIntro
