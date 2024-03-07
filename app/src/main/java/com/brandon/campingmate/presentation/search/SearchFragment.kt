ecf13b2b613b089ee8653bfb8e4a331b191478f7
WjLeeeee <dldnwls0115@naver.com>
2024-03-07 17:35:53 +0900

    [Design] 검색바 활성화시 디자인변경
    텍스트가 있으면 텍스트 지우기 활성화, 엔터키로 검색

--- app/src/main/java/com/brandon/campingmate/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/brandon/campingmate/presentation/search/SearchFragment.kt
@@ -1,9 +1,15 @@
 package com.brandon.campingmate.presentation.search
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -57,8 +63,9 @@ class SearchFragment : Fragment() {
     }
 
     private fun initView() = with(binding) {
-        bottomSheet()
-        scrollTab()
+        bottomSheet() //바텀시트 연결
+        scrollTab() //바텀시트 스크롤탭
+        //리사이클러뷰 연결
         recyclerView.adapter = listAdapter
         recyclerView.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
@@ -115,12 +122,18 @@ class SearchFragment : Fragment() {
             R.id.chipGravel,
             R.id.chipSoil
         )
+        /**
+         * 초기화버튼 클릭시 칩 초기화
+         */
         btnReset.setOnClickListener {
             for (chipId in chipIds) {
                 val chip = root.findViewById<Chip>(chipId)
                 chip.isChecked = false
             }
         }
+        /**
+         * 적용하기 버튼 클릭시 firestore필터링 후 검색
+         */
         btnApply.setOnClickListener {
             doNmList.clear()
             activatedChips.clear()
@@ -176,11 +189,71 @@ class SearchFragment : Fragment() {
             viewModel.callData()
             behavior.state = BottomSheetBehavior.STATE_HIDDEN
         }
-        binding.ivSearch.setOnClickListener {
-            val searchText = binding.tvEdit.text.toString()
-            viewModel.setUpParkParameter(searchText)
+        /**
+         * 엔터누를시 검색
+         */
+        tvEdit.setOnKeyListener{_, KeyCode, event ->
+            if((event.action==KeyEvent.ACTION_DOWN) && (KeyCode==KeyEvent.KEYCODE_ENTER)){
+                val searchText = binding.tvEdit.text.toString()
+                viewModel.setUpParkParameter(searchText)
+                binding.root.hideKeyboard()
+                return@setOnKeyListener true
+            } else{
+                return@setOnKeyListener false
+            }
+        }
+        /**
+         * 텍스트 변화감지
+         */
+        tvEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // 텍스트 변경 전 동작
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // 텍스트가 변경될 때 동작
+                updateImage()
+            }
+            override fun afterTextChanged(s: Editable?) {
+                // 텍스트 변경 후 동작
+            }
+        })
+        /**
+         * editText포커스될때 검색이미지 invisible
+         */
+        tvEdit.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if(hasFocus){
+                ivSearch.visibility = View.INVISIBLE
+            } else{
+                ivSearch.visibility = View.VISIBLE
+            }
+        }
+        ivDelete.setOnClickListener {
+            tvEdit.text.clear()
         }
     }
+
+    /**
+     * x이미지 활성화/비활성화
+     */
+    fun updateImage(){
+        if(binding.tvEdit.text.toString().isNotBlank()){
+            binding.ivDelete.visibility = View.VISIBLE
+        } else{
+            binding.ivDelete.visibility = View.INVISIBLE
+        }
+    }
+
+    /**
+     * 키보드 숨김처리
+     */
+    private fun View.hideKeyboard(){
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+    /**
+     * 바텀시트 스크롤
+     */
     private fun scrollTab() =with(binding){
         searchType.setOnClickListener {
             scrollToView(tvSearchType)
@@ -202,10 +275,13 @@ class SearchFragment : Fragment() {
         }
     }
 
+    /**
+     * 바텀시트 연결
+     */
     private fun bottomSheet() {
 
         behavior = BottomSheetBehavior.from(binding.bottomSheet)
-        behavior.isHideable = true //이게 없었다.
+        behavior.isHideable = true //초기 숨김상태
         behavior.state = BottomSheetBehavior.STATE_HIDDEN // 초기 상태 설정
 
         binding.ivSetting.setOnClickListener {
