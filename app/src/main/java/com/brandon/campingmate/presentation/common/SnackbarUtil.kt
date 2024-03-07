43cb8c968c4f0f9e24e7bc5e06b9f43bc01e307d
avo890 <wizhey7327@gmail.com>
2024-03-07 10:32:55 +0900

    [Feat] 로그인 스낵바 구현
    - 비로그인 시 클릭하면 로그인 액티비티로 이동하는 스낵바 구현
    - SnackbarUtil.showSnackBar(해당view) 코드로 사용가능

---
+++ app/src/main/java/com/brandon/campingmate/presentation/common/SnackbarUtil.kt
@@ -0,0 +1,39 @@
+package com.brandon.campingmate.presentation.common
+
+import android.content.Intent
+import android.graphics.Color
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.brandon.campingmate.R
+import com.brandon.campingmate.presentation.login.LoginActivity
+import com.google.android.material.snackbar.Snackbar
+
+object SnackbarUtil {
+
+    fun showSnackBar(view: View) {
+        val snackbar = Snackbar.make(view, "", 5000)
+
+        val inflater = LayoutInflater.from(view.context)
+        val customLayout = inflater.inflate(R.layout.dialog_snackbar, null)
+
+        val snackbarView = snackbar.view
+        val snackbarLayout = snackbarView as Snackbar.SnackbarLayout
+        snackbarLayout.addView(customLayout, 0)
+
+        snackbarView.setBackgroundColor(Color.TRANSPARENT)
+
+        snackbar.show()
+
+        val textViewGoLogin = snackbarLayout.findViewById<TextView>(R.id.tv_go_login)
+        textViewGoLogin?.setOnClickListener {
+            val intent = Intent(view.context, LoginActivity::class.java)
+            view.context.startActivity(intent)
+        }
+
+
+    }
+
+}
