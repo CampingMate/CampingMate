ea802ade905a4b63a9124a336f8d12fec1997508
quinones <127173772+WjLeeeee@users.noreply.github.com>
2024-03-14 10:08:08 +0900

    [Fix] 무한스크롤 해결 (#82)
    * [Feat] Search무한스크롤 구현
    필터링후 데이터검색시 5개씩 무한스크롤 구현
    
    * [Refactor] campDetail로 넘어갈때 id만 넘기기
    캠프 디테일로 넘어갈때 객체가 아닌 id만 넘기고 받아온id로 검색
    
    * [Fix] 무한스크롤 안되던 오류 해결
    Search에서 무한스크롤 안되던 오류 해결
    
    * [Design] CampDetail ui design
    캠프디테일 댓글부분 ui구성
    
    * [Design] CampDetail 엔티티생성 및 어뎁터 생성
    댓글 보여줄 리사이클러뷰 어뎁터 생성

--- app/src/main/java/com/brandon/campingmate/presentation/search/adapter/SearchListAdapter.kt
+++ app/src/main/java/com/brandon/campingmate/presentation/search/adapter/SearchListAdapter.kt
@@ -1,13 +1,10 @@
-package com.brandon.campingmate.presentation.search
+package com.brandon.campingmate.presentation.search.adapter
 
 import android.content.Intent
-import android.graphics.drawable.ColorDrawable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -16,18 +13,13 @@ import com.brandon.campingmate.domain.model.CampEntity
 import com.brandon.campingmate.databinding.ItemBigCampBinding
 import com.brandon.campingmate.presentation.campdetail.CampDetailActivity
 import com.bumptech.glide.Glide
-import kotlin.math.roundToInt
 
 class SearchListAdapter : ListAdapter<CampEntity, SearchListAdapter.SearchViewHolder>(
     object : DiffUtil.ItemCallback<CampEntity>() {
         override fun areItemsTheSame(
             oldItem: CampEntity,
             newItem: CampEntity
-        ): Boolean = if (oldItem is CampEntity && newItem is CampEntity) {
-            oldItem.contentId == newItem.contentId
-        } else {
-            oldItem == newItem
-        }
+        ): Boolean = oldItem.contentId == newItem.contentId
 
         override fun areContentsTheSame(
             oldItem: CampEntity,
@@ -45,6 +37,7 @@ class SearchListAdapter : ListAdapter<CampEntity, SearchListAdapter.SearchViewHo
         )
 
     override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        Log.d("Search", "onBindViewHolder - Position: $position")
         holder.onBind(getItem(position))
     }
 
@@ -52,6 +45,7 @@ class SearchListAdapter : ListAdapter<CampEntity, SearchListAdapter.SearchViewHo
         private val binding: ItemBigCampBinding,
     ) : SearchViewHolder(binding.root) {
         override fun onBind(item: CampEntity) = with(binding) {
+            Log.d("Search", "onBind - Item: $item")
             if (item !is CampEntity) {
                 return@with
             }
@@ -80,38 +74,9 @@ class SearchListAdapter : ListAdapter<CampEntity, SearchListAdapter.SearchViewHo
             ivBigItem.clipToOutline = true
 
             binding.root.setOnClickListener {
-                val myData = CampEntity(
-                    addr1 = item.addr1,
-                    contentId = item.contentId,
-                    facltNm = item.facltNm,
-                    wtrplCo = item.wtrplCo,
-                    brazierCl = item.brazierCl,
-                    sbrsCl = item.sbrsCl,
-                    posblFcltyCl = item.posblFcltyCl,
-                    hvofBgnde = item.hvofBgnde,
-                    hvofEnddle = item.hvofEnddle,
-                    toiletCo = item.toiletCo,
-                    swrmCo = item.swrmCo,
-                    featureNm = item.featureNm,
-                    induty = item.induty,
-                    tel = item.tel,
-                    homepage = item.homepage,
-                    resveCl = item.resveCl,
-                    siteBottomCl1 = item.siteBottomCl1,
-                    siteBottomCl2 = item.siteBottomCl2,
-                    siteBottomCl3 = item.siteBottomCl3,
-                    siteBottomCl4 = item.siteBottomCl4,
-                    siteBottomCl5 = item.siteBottomCl5,
-                    glampInnerFclty = item.glampInnerFclty,
-                    caravInnerFclty = item.caravInnerFclty,
-                    intro = item.intro,
-                    themaEnvrnCl = item.themaEnvrnCl
-                )
                 val myId = item.contentId
-
                 val intent = Intent(binding.root.context, CampDetailActivity::class.java).apply {
-                    putExtra("campData", myData)
-                    putExtra("campId", myId)
+                    putExtra("campData", myId)
                 }
                 binding.root.context.startActivity(intent)
             }
