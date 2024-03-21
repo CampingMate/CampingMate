49d97b87059b2b9642dd7f07aa54395fa06b586b
quinones <127173772+WjLeeeee@users.noreply.github.com>
2024-03-21 22:11:26 +0900

    [Fix] 댓글창 새로고침 (#156)
    댓글 작성후 최근댓글 새로고침 안되던 문제 해결

--- app/src/main/java/com/brandon/campingmate/presentation/campdetail/CampDetailActivity.kt
+++ app/src/main/java/com/brandon/campingmate/presentation/campdetail/CampDetailActivity.kt
@@ -290,6 +290,17 @@ class CampDetailActivity : AppCompatActivity(), OnMapReadyCallback {
         behavior = BottomSheetBehavior.from(bottomSheet)
         behavior.isHideable = true
         behavior.state = BottomSheetBehavior.STATE_HIDDEN
+
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback(){
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if(newState == BottomSheetBehavior.STATE_HIDDEN){
+                    viewModel.checkComment(myId!!)
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
     }
 
     private fun scrollListener() = with(binding){
