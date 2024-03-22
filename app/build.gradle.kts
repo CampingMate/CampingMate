9f479f413dbe60b1cea687b3af0111c0b7d29fda
avo890 <wizhey7327@gmail.com>
2024-03-22 17:54:35 +0900

    [Feat] 구글 로그인 연결
    - 구글 로그인 버튼을 통해 구글 로그인 연결
    - 유저데이터 이미지 업로드 시 비동기 작업이 원활하게 이루어지도록 profileImgUpload() 리팩토링
    - 구글 로그아웃 연결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     implementation("com.google.firebase:firebase-database-ktx:20.3.0")
     implementation("com.google.firebase:firebase-firestore:24.10.2")
     implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
 
     // Retrofit & OkHttp
     implementation("com.google.code.gson:gson:2.10.1")
