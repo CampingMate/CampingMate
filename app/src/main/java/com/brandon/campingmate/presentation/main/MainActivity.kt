07dffc62dfb22bd2805463fefb186f2770e7ae5e
SoungHyun Choi <sounghyun35@naver.com>
2024-03-24 17:50:29 +0900

    [Design] 디자인 피드백 반영

--- app/src/main/java/com/brandon/campingmate/presentation/main/MainActivity.kt
+++ app/src/main/java/com/brandon/campingmate/presentation/main/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.brandon.campingmate.presentation.main
 
 import android.animation.ObjectAnimator
-import android.animation.PropertyValuesHolder
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.view.animation.AnticipateInterpolator
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -14,14 +13,7 @@ import com.brandon.campingmate.R
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.asLiveData
 import com.brandon.campingmate.databinding.ActivityMainBinding
-import com.brandon.campingmate.domain.model.HomeEntity
-import com.brandon.campingmate.presentation.splash.SplashViewModel
-import com.kakao.sdk.common.util.Utility
-import nl.joery.animatedbottombar.AnimatedBottomBar
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -30,33 +22,25 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var splashScreen: SplashScreen
 
-    private val viewModel by lazy {
-        ViewModelProvider(this)[SplashViewModel::class.java]
+    private var backPressedTime: Long = 0
+    @SuppressLint("MissingSuperCall")
+    override fun onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            finish()
+        } else {
+            Toast.makeText(this, "한 번 더 누르면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
     }
-
-//    private var isDataLoaded = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("Main", "#csh onCreate")
-
-//        installSplashScreen().apply {
-//            Log.d("Main", "#csh installSplashScreen")
-//            setKeepOnScreenCondition{viewModel.isLoading.value}
-//        }
         splashScreen = installSplashScreen()
         startSplash()
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-//        var keyHash = Utility.getKeyHash(this)
-//        Log.d("keyHash", "$keyHash")
-
         Timber.plant(Timber.DebugTree())
-
-//        homeCity = intent.getParcelableArrayListExtra("homeCity") ?: arrayListOf()
-//        homeTheme = intent.getParcelableArrayListExtra("homeTheme")?: arrayListOf()
-//        Log.d("Main","#csh homeCity: $homeCity")
-//        Log.d("Main","#csh homeTheme: $homeTheme")
 
         initView()
 
@@ -72,20 +56,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startSplash() {
-//            viewModel.isGet.asLiveData().observe(this) { isDataLoaded ->
-//                val isCityLoaded = isDataLoaded["city"]?:false
-//                val isThemeLoaded = isDataLoaded["theme"]?:false
-//                if (isCityLoaded&&isThemeLoaded) {
-//                    splashScreen.setKeepOnScreenCondition { viewModel.isLoading.value }
-//            }
-//        }
-//        viewModel.loadData()
 
         splashScreen.setOnExitAnimationListener { splashScreenView ->
-            val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1f, 5f, 1f)
-            val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f, 5f, 1f)
-
-//            ObjectAnimator.ofPropertyValuesHolder(splashScreenView.iconView, scaleX, scaleY).run {
             ObjectAnimator.ofPropertyValuesHolder(splashScreenView.iconView).run {
                 interpolator = AnticipateInterpolator()
                 duration = 1000L
@@ -97,26 +69,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-//    private val isGetDataObserver = Observer<Int> { loadedCount ->
-//        if (loadedCount == 2 && !isDataLoaded) { // 데이터를 가져오고, 로딩이 완료되지 않았을 때만 실행
-//            handleDataLoaded()
-//        }
-//    }
-
-//    private fun handleDataLoaded(){
-//        isDataLoaded = true // 데이터가 로딩되었음을 표시
-//        homeCity = viewModel.allCityData
-//        homeTheme = viewModel.allThemeData
-////        startActivity(Intent(this@MainActivity, MainActivity::class.java))
-////        finish() // 현재 액티비티 종료
-//    }
-
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        // Activity가 종료될 때 LiveData의 observe를 제거하여 메모리 누수를 방지
-//        viewModel.isGet.removeObserver(isGetDataObserver)
-//    }
 
 }
 
