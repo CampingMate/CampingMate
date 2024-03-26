b56c5ca00841559f14a88508742ef68f90b25dc4
WjLeeeee <dldnwls0115@naver.com>
2024-03-26 15:18:31 +0900

    [Fix] 암호화시 키값 달라서 재실행후 안가져와지던 문제 해결

--- app/src/main/java/com/brandon/campingmate/utils/UserCryptoUtils.kt
+++ app/src/main/java/com/brandon/campingmate/utils/UserCryptoUtils.kt
@@ -10,7 +10,8 @@ import javax.crypto.spec.SecretKeySpec
 
 object UserCryptoUtils {
     // AES 암호화에 사용할 무작위로 생성된 256비트 키
-    val AES_KEY: ByteArray = generateRandomAESKey()
+    const val AES_KEY = "abcdefghijklmnopabcdefghijklmnop"
+//    val AES_KEY: ByteArray = generateRandomAESKey()
     private fun generateRandomAESKey(): ByteArray {
         val keyGen = KeyGenerator.getInstance("AES")
         keyGen.init(256, SecureRandom())
@@ -18,20 +19,20 @@ object UserCryptoUtils {
         return secretKey.encoded
     }
     // 대칭키 암호화 함수
-    fun encrypt(data: String, key: ByteArray): String {
+    fun encrypt(data: String, key: String): String {
         //AES알고리즘 사용해서 ECB모드와 PKCS5Padding패딩방식을 지정
         //ECB모드는 암호화할 블록이 서로 독립적으로 처리되는 방식
         //PKCS5Padding은 블록 크기에 맞춰 패딩을 추가하는 방식
         val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
-        val secretKeySpec = SecretKeySpec(key, "AES") //SecretKeySpec클래스 사용해서 암호화 키 생성
+        val secretKeySpec = SecretKeySpec(key.toByteArray(), "AES") //SecretKeySpec클래스 사용해서 암호화 키 생성
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec) //객체를 암호화 모드로 초기화
         val encryptedBytes = cipher.doFinal(data.toByteArray()) //주어진 데이터를 암호화, 바이트배열로 전환
         return Base64.encodeToString(encryptedBytes, Base64.DEFAULT) //Base64로 인코딩해서 문자열로 반환
     }
     // 대칭키 복호화 함수
-    fun decrypt(encryptedData: String, key: ByteArray): String {
+    fun decrypt(encryptedData: String, key: String): String {
         val cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING")
-        val secretKeySpec = SecretKeySpec(key, "AES")
+        val secretKeySpec = SecretKeySpec(key.toByteArray(), "AES")
         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec)
         val decryptedBytes = cipher.doFinal(Base64.decode(encryptedData, Base64.DEFAULT))
         return String(decryptedBytes)
