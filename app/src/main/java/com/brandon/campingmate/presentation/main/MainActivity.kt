9caef54883f04943c16d302d568e4759a59414a3
Keunyoung Song <rmsdud623@gmail.com>
2024-03-27 12:11:24 +0900

    [Feat] 인터넷 연결 없는 상태 대응 (#206)

--- app/src/main/java/com/brandon/campingmate/presentation/main/MainActivity.kt
+++ app/src/main/java/com/brandon/campingmate/presentation/main/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.brandon.campingmate.presentation.main
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -8,18 +13,21 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.brandon.campingmate.R
 import com.brandon.campingmate.databinding.ActivityMainBinding
 import com.brandon.campingmate.presentation.splash.SplashViewModel
+import com.brandon.campingmate.utils.isInternetAvailable
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val sharedViewModel: SplashViewModel by viewModels()
-
+    private lateinit var connectivityManager: ConnectivityManager
+    private lateinit var networkCallback: ConnectivityManager.NetworkCallback
 
     private var backPressedTime: Long = 0
 
@@ -39,8 +47,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        checkInternetConnection()
+
         Timber.plant(Timber.DebugTree())
+        setupNetworkCallback()
         initView()
+    }
+
+    private fun checkInternetConnection() = with(binding) {
+        if (!isInternetAvailable(this@MainActivity)) {
+            Timber.d("인터넷 연결이 필요합니다.")
+            clNoNetwork.isVisible = true
+        } else {
+            clNoNetwork.isVisible = false
+        }
+    }
+
+    private fun setupNetworkCallback() {
+        connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                Timber.tag("NETWORK").d("인터넷 연결중")
+                runOnUiThread {
+                    binding.clNoNetwork.isVisible = false
+                }
+            }
+
+            override fun onLost(network: Network) {
+                Timber.tag("NETWORK").d("인터넷 실패")
+                runOnUiThread {
+                    binding.clNoNetwork.isVisible = true
+                }
+            }
+        }
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (::connectivityManager.isInitialized) {
+            connectivityManager.unregisterNetworkCallback(networkCallback)
+        }
     }
 
     private fun setSplash() {
